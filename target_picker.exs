1c31fb2c2dcff9ef2a61dc6f5d5c48d2fdd5009b
PhilipCesani <76191296+PhilipCesani@users.noreply.github.com>
2023-06-29 16:58:54 -0500

    Add files via upload

--- target_picker.exs
+++ target_picker.exs
@@ -21,17 +21,24 @@ defmodule TargetPicker do
 
   defp build_paths(word_list) do
     Enum.map(word_list, fn word ->
-      build_paths_for_word(word, word_list)
+      build_paths_for_word(word, word_list, [word])
+      |> IO.inspect()
     end)
   end
 
-  defp build_paths_for_word(word, word_list) do
+  defp build_paths_for_word(nil, word_list, current_path) do
+    Enum.reverse(current_path)
+  end
+
+  defp build_paths_for_word(word, word_list, current_path) do
     new_word = Enum.find(word_list, fn new_word ->
-      difference_in_chars(new_word, word) == 1
+      difference_in_chars(new_word, word) == 1 && new_word not in current_path
     end)
 
     IO.inspect(word, label: "word")
     IO.inspect(new_word, label: "new_word")
+
+    build_paths_for_word(new_word, word_list, [new_word | current_path])
   end
 
   defp difference_in_chars(word_1, word_2) do
